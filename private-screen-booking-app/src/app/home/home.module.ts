41f23dac698531108545c373ff9f5c9dbe611fa0
karthikdev0 <karthik.kc.dev@gmail.com>
2022-09-13 21:38:07 +0530

    created home module and 3 components inside it

---
+++ private-screen-booking-app/src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { ImageSliderComponent } from './image-slider/image-slider.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
+
+@NgModule({
+  declarations: [
+    ImageSliderComponent,
+    ReviewsComponent,
+    ContactUsComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
